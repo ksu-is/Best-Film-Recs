3e4134b62c63ebbe16fb839ace0c3befb71f6dfb
andyisdiaz <adiaz49@students.kennesaw.edu>
2025-11-21 19:32:02 -0500

    Added File Categories Dictionary

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -1,1 +1,14 @@
 import os
+import shutil
+
+#File Categories Dictionary
+file_categories = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff"],
+    "Videos": [".mp4", ".mkv", ".flv", ".avi", ".mov"],
+    "Documents": [".pdf", ".doc", ".docx", ".txt", ".ppt", ".pptx", ".xls", ".xlsx"],
+    "Audio": [".mp3", ".wav", ".aac", ".flac"],
+    "Archives": [".zip", ".rar", ".7z", ".tar", ".gz"],
+    "Data": [".csv", ".json", ".xml"],
+    "Others": []
+
+}
